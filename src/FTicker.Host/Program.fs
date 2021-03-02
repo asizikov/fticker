5f145a4b27a1a1f025a46b67203454695de825db
Anton Sizikov <sizikov.anton@gmail.com>
2021-03-02 22:41:42 +0100

    containerize the service

---
+++ src/FTicker.Host/Program.fs
@@ -0,0 +1,41 @@
+﻿open System.IO
+open FTicker.Host.Services.TickingService
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+
+let setBasePath (path: string) (configuration : IConfigurationBuilder) =
+    configuration.SetBasePath(path)
+    
+let addOptionalJson (path: string) (configuration : IConfigurationBuilder) =
+    configuration.AddJsonFile(path, false)
+let addEnvVars (configuration : IConfigurationBuilder) =
+    configuration.AddEnvironmentVariables()
+
+let configureHost argv  =
+    let hostBuilder = Host.CreateDefaultBuilder(argv)
+    hostBuilder.ConfigureAppConfiguration( fun hostContest configuration ->
+            configuration.Sources.Clear()
+            configuration 
+                |> setBasePath( Directory.GetCurrentDirectory()) 
+                |> addOptionalJson "appsettings.json" 
+                |> addEnvVars
+                |> ignore
+            ) |> ignore
+        
+    hostBuilder.ConfigureServices (fun hostContext services ->
+            services.AddHostedService<TickingHostedService>() |> ignore
+            ) |> ignore
+    hostBuilder.Build()
+    
+[<EntryPoint>]
+let main argv =
+    async {
+        do! Async.SwitchToThreadPool ()
+        use host = configureHost argv        
+        let! _ = host.StartAsync() |> Async.AwaitTask
+        
+        return! host.WaitForShutdownAsync() |> Async.AwaitTask
+        
+    } |> Async.RunSynchronously
+    0 
---
+++ src/FTicker.Host/Services/TickingService.fs
@@ -0,0 +1,17 @@
+﻿module FTicker.Host.Services.TickingService
+open System
+open System.Threading
+open System.Threading.Tasks
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+type TickingHostedService (logger: ILogger<TickingHostedService>) =
+    inherit BackgroundService()
+       
+    override this.ExecuteAsync(stoppingToken :CancellationToken) =
+        "starting background process" |> logger.LogInformation 
+        async {
+            while( not stoppingToken.IsCancellationRequested) do 
+                $"tick at {DateTime.Now}" |> logger.LogInformation
+                do! Async.Sleep 1000
+        } |> Async.StartAsTask :> Task
